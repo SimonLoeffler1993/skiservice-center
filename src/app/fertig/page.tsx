15c62e1e1f5abde175900c72a4b844ee22331848
Simon <nomis3@gmx.de>
2026-06-05 21:53:35 +0200

    fertigstellen zu dynamisch

--- src/app/fertig/page.tsx
+++ src/app/fertig/page.tsx
@@ -1,10 +1,11 @@
 import SkiserviceFertigScanner from "@/components/skiservice/fertigstellen/scanner"
 import { getConfig } from "@/lib/config"
 
+export const dynamic = 'force-dynamic'; 
 
 export default function SkiServiceFertig() {
     const config = getConfig();
-
+    console.log("Backend URL:", config.backendUrl);
     return (
         <>
             <SkiserviceFertigScanner backendUrl={config.backendUrl} />
