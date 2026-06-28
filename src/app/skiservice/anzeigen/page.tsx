9c98c27b25b66751e2cd2e062e5765bef56ba06a
Simon <nomis3@gmx.de>
2026-06-28 21:29:10 +0200

    Saisonwechsler auf Service Liste Seite

--- src/app/skiservice/anzeigen/page.tsx
+++ src/app/skiservice/anzeigen/page.tsx
@@ -1,5 +1,47 @@
-export default function SkiservicesAnzeigen(){
-    return(
-        <p>Hier werden mal die Service angezeigt</p>
+import SaisonWechseler from "@/components/saisonverleih/liste/SaisonWechseler";
+import { getAktuelleSaisonID, getSaisons } from "@/lib/saisonactions";
+import { Snowflake } from "lucide-react";
+import { Suspense } from "react";
+
+type SkiservicesAnzeigenProps = {
+    searchParams: Promise<{ saison?: string }>
+}
+
+export default async function SkiservicesAnzeigen({ searchParams }: SkiservicesAnzeigenProps) {
+
+    // Daten für den Saisonwechsler laden
+    const saisonData = await getSaisons();
+    const { saison } = await searchParams;
+
+    // atuelle Saison
+    let saisonID: number | undefined
+
+    // Saison aus URL, falls nicht vorhanden wird die aktuelle ermittelt
+    if (!saison) {
+        const aktuelleSaison = await getAktuelleSaisonID()
+        if (aktuelleSaison) {
+            saisonID = aktuelleSaison.ID
+        }
+    } else {
+        saisonID = parseInt(saison)
+    }
+
+    // Fehler falls keine Saison gibt
+    if (!saisonID) {
+        return <p>Keine Saison gefunden!</p>
+    }
+    return (
+        <section className="space-y-4">
+            <div className="flex items-center justify-between gap-2">
+                <div className="flex items-center gap-2">
+                    <Snowflake className="h-5 w-5 text-muted-foreground" />
+                    <h1 className="text-xl font-semibold">Skiservice</h1>
+                </div>
+            </div>
+            <Suspense fallback={<div>Lade Skiservices ...</div>}>
+                <SaisonWechseler saisons={saisonData} selectedSaisonID={saisonID} />
+            </Suspense>
+
+        </section>
     )
 }
