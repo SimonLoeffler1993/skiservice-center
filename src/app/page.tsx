0be5e8a29656c31ce83fd80e2ceb68f49138a0f5
Simon <nomis3@gmx.de>
2026-08-17 22:26:33 +0200

    Saison beim ersten Start anlegen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
 import Kachel from "@/components/startseite/kachel";
 import MenueLeiste from "@/components/saisonverleih/menue/MenueLeiste";
-import AktuelleSaison from "@/components/AktuelleSaison";
+import AktuelleSaison from "@/components/saison/AktuelleSaison";
+import { getSaisons } from "@/lib/saisonactions";
 
 export default async function Home() {
+
+    const alleSaisons = await getSaisons();
+
+    if (alleSaisons.length === 0) {
+        return (
+            <>
+                <MenueLeiste />
+                <div className="min-h-screen flex flex-col gap-2 items-center justify-center bg-gray-100">
+                    <h1 className="text-3xl font-bold underline">Skiservice Center</h1>
+                    <AktuelleSaison saison={undefined} />
+                </div>
+            </>
+        );
+    }
+
+    const aktuelleSaison = alleSaisons.find(saison => saison.Verwendet === 1);
+
     return (
         <>
             <MenueLeiste />
             <div className="min-h-screen flex flex-col gap-2 items-center justify-center bg-gray-100">
                 <h1 className="text-3xl font-bold underline">Skiservice Center</h1>
-                <AktuelleSaison />
+                <AktuelleSaison saison={aktuelleSaison} />
                 <h2 className="text-2xl">was soll gemacht werden?</h2>
                 <Kachel title="Saisonverleih" description="Es wird ein Saisonverleih erfasst." href="/saisonverleih/erstellen" buttonText="Erstellen" />
                 <Kachel title="Service" description="Es wird ein Saisonverleih erfasst." href="/skiservice/erstellen" buttonText="Erstellen" />
